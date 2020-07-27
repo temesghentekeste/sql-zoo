ac15ebef9645626bcef4eebbfea8f3e22a897b87
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2020-07-27 23:40:27 +0300

    Complete join operations

--- 05_sum_count.sql
+++ 05_sum_count.sql
@@ -13,10 +13,12 @@ SELECT SUM(gdp)
 FROM world
 WHERE continent = 'Africa'
 
+
 -- How many countries have an area of at least 1000000
 SELECT COUNT(*)
 FROM world
 WHERE area > 1000000
+
 
 -- What is the total population of ('Estonia', 'Latvia', 'Lithuania')
 SELECT SUM(population) 
@@ -30,11 +32,13 @@ SELECT continent, count(*) AS 'countries'
 FROM world
 GROUP BY continent
 
+
 -- For each continent show the continent and number of countries with populations of at least 10 million.
 SELECT continent, COUNT(*) 
 FROM world
 WHERE population >= 10000000
 GROUP BY continent
+
 
 -- List the continents that have a total population of at least 100 million.
 SELECT continent FROM 
